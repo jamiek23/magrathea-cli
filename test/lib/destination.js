17ea639013165a703ada6784c52418bd0713ad5c
Jamie Knight <jamie@ipcortex.co.uk>
2017-02-22 13:31:44 +0000

    Added parse function, plus a few more fixes for the destination class.

--- test/lib/destination.js
+++ test/lib/destination.js
@@ -7,6 +7,53 @@ describe('Destination', function() {
 		dest = new Destination();
 	});
 
+	describe('.constructor()', function() {
+		it('creates an instance from an object', function() {
+			dest = new Destination({
+				type: Destination.types.sip,
+				host: 'sip.mydomain.example',
+				username: 'sipuser'
+			});
+			assert(dest instanceof Destination);
+			assert.equal(dest.type, Destination.types.sip);
+			assert.equal(dest.host, 'sip.mydomain.example');
+			assert.equal(dest.username, 'sipuser');
+		});
+		it('creates an instance from a string', function() {
+			dest = new Destination('S:user@sip.mydomain');
+			assert(dest instanceof Destination);
+			assert.equal(dest.type, Destination.types.sip);
+			assert.equal(dest.username, 'user');
+			assert.equal(dest.username, 'user');
+		});
+	});
+
+	describe('.parse()', function() {
+		it('creates an object from a telephone number', function() {
+			obj = Destination.parse('441234789567');
+			assert.equal(obj.type, Destination.types.telephone);
+			assert.equal(obj.number, '441234789567');
+		});
+		it('creates an object from a SIP string', function() {
+			obj = Destination.parse('S:user@sip.mydomain');
+			assert.equal(obj.type, Destination.types.sip);
+			assert.equal(obj.username, 'user');
+			assert.equal(obj.password, null);
+			assert.equal(obj.host, 'sip.mydomain');
+		});
+		it('creates an object from a SIP string with password', function() {
+			obj = Destination.parse('S:user:pass@sip.mydomain');
+			assert.equal(obj.type, Destination.types.sip);
+			assert.equal(obj.username, 'user');
+			assert.equal(obj.password, 'pass');
+			assert.equal(obj.host, 'sip.mydomain');
+		});
+		it('copes with an empty string', function() {
+			obj = Destination.parse('');
+			assert(Object.keys(obj).length === 0);
+		});
+	});
+
 	describe('#toString()', function() {
 		it('exists', function() {
 			assert.equal(typeof dest.toString, 'function');
@@ -19,14 +66,14 @@ describe('Destination', function() {
 		it('correctly stringifies SIP type', function() {
 			dest.type = Destination.types.sip;
 			dest.username = 'myuser';
-			dest.hostname = 'sip.mydomain.example';
+			dest.host = 'sip.mydomain.example';
 			assert.equal(dest.toString(), 'S:myuser@sip.mydomain.example');
 		});
 		it('correctly stringifies SIP type with password', function() {
 			dest.type = Destination.types.sip;
 			dest.username = 'myuser';
 			dest.password = 'pass';
-			dest.hostname = 'sip.mydomain.example';
+			dest.host = 'sip.mydomain.example';
 			assert.equal(dest.toString(), 'S:myuser:pass@sip.mydomain.example');
 		});
 	});
